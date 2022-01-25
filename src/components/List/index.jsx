8d5926d0c2daf5a70986ca6eae65b0d6bf8ec7cd
alex2075 <gagara5620@gmail.com>
2022-01-25 08:49:28 +0600

    add todo

---
+++ src/components/List/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./List.scss";
+import classNames from "classnames";
+import Badge from "../Badge";
+
+import removeSvg from "../../assets/img/remove.svg";
+
+const List = ({ items, isRemovable, onClick, onRemove }) => {
+  const removeList = (item) => {
+    if (window.confirm("Вы действительно хотите удалить? ")) {
+      onRemove(item);
+    }
+  };
+
+  return (
+    <ul onClick={onClick} className="list">
+      {items.map((item, index) => (
+        <li
+          key={index}
+          className={classNames(item.className, { active: item.active })}
+        >
+          <i>{item.icon ? item.icon : <Badge color={item.color} />}</i>
+          <span>{item.name}</span>
+          {isRemovable && (
+            <img
+              className="list__remove-icon"
+              src={removeSvg}
+              alt="Remove icon"
+              onClick={() => removeList(item)}
+            />
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default List;
